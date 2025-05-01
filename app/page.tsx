d6f3a2841ebdf5c064b7e5dc125d5c08d71f9c55
suulayman5 <ajahsuleiman@gmail.com>
2025-05-01 15:11:21 +0100

    Enhance form validation and user feedback in Airtime purchase form

--- app/page.tsx
+++ app/page.tsx
@@ -46,11 +46,24 @@ export default function App() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!/^\d{10,15}$/.test(form.phone)) return setMessage('Invalid phone number.');
-    if (!form.network || !form.amount) return setMessage('All fields are required.');
-    if (Number(form.amount) <= 0) return setMessage('Amount must be greater than 0.');
+  
+    const { phone, network, amount } = form;
+    const amountNumber = Number(amount);
+  
+    if (!/^\d{11,12}$/.test(phone)) {
+      return toast.error(' Invalid phone number. It must be minimum of 11 digits.');
+    }
+  
+    if (!network) {
+      return toast.error('Please select a mobile network.');
+    }
+  
+    if (!amount || isNaN(amountNumber) || amountNumber <= 50) {
+      return toast.error('Minimum amount is ₦ 50.');
+    }
     mutate();
   };
+  
 
   return (
     <>
@@ -59,13 +72,16 @@ export default function App() {
         <div className="max-w-sm md:max-w-md p-4 bg-transparent  rounded-xl shadow-2xl">
           <h1 className="text-2xl text-center p-5 font-bold mb-4">Buy Airtime</h1>
           <form onSubmit={handleSubmit} className="space-y-4">
-            <input
-              type="tel"
-              placeholder="Phone Number"
-              value={form.phone}
-              onChange={(e) => setForm({ ...form, phone: e.target.value })}
-              className="w-full p-2 border border-gray-300 rounded text-black"
-            />
+          <input
+            type="tel"
+            placeholder="Phone Number"
+            value={form.phone}
+            onChange={(e) => setForm({ ...form, phone: e.target.value })}
+            className={`w-full p-2 border rounded text-black ${
+              !/^\d{10,15}$/.test(form.phone) && form.phone ? 'border-red-500' : 'border-gray-300'
+            }`}
+          />
+
             <select
               value={form.network}
               onChange={(e) => setForm({ ...form, network: e.target.value })}
@@ -82,14 +98,17 @@ export default function App() {
                   ) : null
                 )}
               </select>
+              <div className="flex relative">
+              <p className="absolute left-3 top-2 text-gray-500">₦</p>
 
             <input
               type="number"
               placeholder="Amount"
               value={form.amount}
               onChange={(e) => setForm({ ...form, amount: e.target.value })}
-              className="w-full p-2 border border-gray-300 rounded text-black"
+              className="w-full p-2 pl-8 border border-gray-300 rounded text-black "
             />
+              </div>
             <button
               type="submit"
               disabled={isPending}
@@ -125,6 +144,7 @@ export default function App() {
                 'Submit'
               )}
             </button>
+            {message && <p className="text-red-500 text-center">{message}</p>}
           </form>
         </div>
       </div>
